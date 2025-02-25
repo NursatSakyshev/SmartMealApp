195fad0198ff96cf0d192ba072d3de359c55c668
Nursat2022 <nursatsakyshev2003@gmail.com>
2025-02-25 20:05:37 +0500

    first commit

---
+++ SmartMeal/SmartMeal/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SmartMeal
+//
+//  Created by Nursat Sakyshev on 25.02.2025.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        return true
+    }
+
+}
+
---
+++ SmartMeal/SmartMeal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartMeal
+//
+//  Created by Nursat Sakyshev on 25.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
