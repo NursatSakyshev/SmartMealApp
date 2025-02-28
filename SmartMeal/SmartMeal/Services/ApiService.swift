364e1f94d901c8dcf7796601040116ac4a87d9aa
Nursat2022 <nursatsakyshev2003@gmail.com>
2025-02-28 11:46:49 +0500

    ApiService with local db

--- SmartMeal/SmartMeal/Services/ApiService.swift
+++ SmartMeal/SmartMeal/Services/ApiService.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class APIService {
+    func getData(completion : @escaping ([[Recipe]]) -> ()) {
+        completion([getRecommendations(), getPopular(), getQuickEasy(), getHealthy()])
+    }
+    
+    func getRecommendations() -> [Recipe] {
+        [
+            Recipe.recipes[0], Recipe.recipes[4], Recipe.recipes[0], Recipe.recipes[1]
+        ]
+    }
+    
+    func getPopular() -> [Recipe] {
+        [
+            Recipe.recipes[0], Recipe.recipes[1]
+        ]
+    }
+    
+    func getQuickEasy() -> [Recipe] {
+        Recipe.recipes
+    }
+    
+    func getHealthy() -> [Recipe] {
+        Recipe.recipes
+    }
+}
+
