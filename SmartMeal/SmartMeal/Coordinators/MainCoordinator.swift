ec2b1d612fd63b57558b2421b5f559e508a42a4f
Nursat2022 <nursatsakyshev2003@gmail.com>
2025-03-30 22:17:57 +0500

    clear

--- SmartMeal/SmartMeal/Coordinators/MainCoordinator.swift
+++ SmartMeal/SmartMeal/Coordinators/MainCoordinator.swift
@@ -24,7 +24,6 @@ class MainCoordinator: NSObject, Coordinator, UINavigationControllerDelegate {
     func start() {
         let hasSeenOnboarding = UserDefaults.standard.bool(forKey: "hasSeenOnboarding")
         let token = UserDefaults.standard.string(forKey: "authToken")
-//        UserDefaults.standard.set(nil, forKey: "authToken")
         
         if !hasSeenOnboarding {
             showOnboardingScreen()
@@ -40,20 +39,6 @@ class MainCoordinator: NSObject, Coordinator, UINavigationControllerDelegate {
                 }
             }
         }
-        
-        
-//        if token == nil {
-//            showAuth()
-//        } else {
-//            navigateToTabBar()
-//            APIService.shared.validateToken { isValid in
-//                DispatchQueue.main.async {
-//                    if !isValid {
-//                        self.showAuth()
-//                    }
-//                }
-//            }
-//        }
     }
     
     func showAuth() {
